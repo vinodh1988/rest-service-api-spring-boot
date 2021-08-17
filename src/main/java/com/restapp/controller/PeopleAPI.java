69e4277286eebc4f56c758aa5a4276d263a98b9c
vinodhkc@vinodhconnects.onmicrosoft.com <vinodhkc@vinodhconnects.onmicrosoft.com>
2021-08-17 11:26:05 +0530

    custom-queries

--- src/main/java/com/restapp/controller/PeopleAPI.java
+++ src/main/java/com/restapp/controller/PeopleAPI.java
@@ -1,9 +1,14 @@
 package com.restapp.controller;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.List;
 
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +18,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rest.util.RecordAlreadyExists;
@@ -77,4 +83,14 @@ public ResponseEntity<String> deletePeople(@PathVariable Integer sno){
 		return new ResponseEntity<>("No Such Record Exists",HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 }
+
+@RequestMapping(
+		  value = "/images/{sno}",
+		  produces = MediaType.IMAGE_PNG_VALUE
+		)
+		public @ResponseBody byte[] getImageWithMediaType(@PathVariable Integer sno) throws IOException {
+	     File f=new File("E:\\images\\pic"+sno+".png");
+		    FileInputStream in = new FileInputStream(f);
+		    return IOUtils.toByteArray(in);
+		}
 }
